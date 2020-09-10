218937e3d8376a36559a990649fccd16377a54ed
AlbertLeeeeee <937749759@qq.com>
2020-09-11 03:09:07 +0800

    整合Freemarker

--- src/main/java/org/javaboy/meeting/controller/HelloController.java
+++ src/main/java/org/javaboy/meeting/controller/HelloController.java
@@ -1,12 +1,14 @@
 package org.javaboy.meeting.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public String hello(Model model) {
+        model.addAttribute("hello","Hello World!!");
+        return "hello";
     }
 }
